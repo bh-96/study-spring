fa52eb1f89d64ff9763548c2d900ea21f0951d72
BH <bh@sweettracker.co.kr>
2021-08-27 14:58:37 +0900

    2021.08.26 codility lesson5 풀이 중

--- src/test/java/com/bh/study/codility/Lesson4_CountingElements.java
+++ src/test/java/com/bh/study/codility/Lesson4_CountingElements.java
@@ -2,18 +2,40 @@ package com.bh.study.codility;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 public class Lesson4_CountingElements {
 
     @Test
     public void test1_FrogRiverOne() {
-
+        int X = 4;
+        int[] A = {1,3,1,4,2,3,5,4};
+        System.out.println(solution(X, A));
     }
 
-    /**
-     * 88% (time out) -> O(N+M)
-     */
+    public int solution(int X, int[] A) {
+        // write your code in Java SE 8
+        int answer = -1;
+
+        int[] visited = new int[X];
+        int n = A.length;
+        int v = 0;
+
+        for (int i = 0; i < n; i++) {
+            int k = A[i];
+
+            if (visited[k - 1] == 0) {
+                visited[k - 1]++;
+                v++;
+            }
+
+            if (v == X) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+
     @Test
     public void test2_MaxCounters() {
         int N = 5;
@@ -32,20 +54,26 @@ public class Lesson4_CountingElements {
 
         for (int x : A) {
             if (x >= 1 && x <= N) {
-                answer[x - 1]++;
                 int k = answer[x - 1];
-                if (max < k) {
-                    max = k;
+
+                if (maxCounter >= k) {
+                    answer[x - 1] = maxCounter + 1;
+                } else {
+                    answer[x - 1]++;
                 }
+
+                max = Math.max(max, answer[x - 1]);
             } else if (x == N + 1) {
-                maxCounter += max;
-                max = 0;
-                answer = new int[N];
+                maxCounter = max;
             }
         }
 
-        for (int i = 0; i < N; i++) {
-            answer[i] += maxCounter;
+        if (maxCounter > 0) {
+            for (int i = 0; i < N; i++) {
+                if (maxCounter > answer[i]) {
+                    answer[i] = maxCounter;
+                }
+            }
         }
 
         return answer;
--- src/test/java/com/bh/study/codility/Lesson5_PrefixSums.java
+++ src/test/java/com/bh/study/codility/Lesson5_PrefixSums.java
@@ -6,11 +6,78 @@ public class Lesson5_PrefixSums {
 
     @Test
     public void test1_PassingCars() {
+        int[] A = {0,1,0,1,1,1,0,1};
+        System.out.println(solution(A));
+    }
 
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int answer = 0;
+
+        int n = A.length;
+
+        if (n == 1) {
+            return answer;
+        }
+
+        int x = 0;
+
+        for (int i = 1; i < n; i++) {
+            int p = A[i - 1];
+            int q = A[i];
+
+            if (p == 0) {
+                x++;
+            }
+
+            if (q == 1) {
+                answer += q * x;
+            }
+        }
+
+        if (answer > 1000000000 || answer < 0) {
+            answer = -1;
+        }
+
+        return answer;
     }
 
     @Test
     public void test2_MinAvgTwoSlice() {
+        int[] A = {4,2,2,5,1,5,8};
+        System.out.println(solution2(A));
+    }
 
+    public int solution2(int[] A) {
+        // write your code in Java SE 8
+        int answer = 0;
+
+        int n = A.length;
+
+        double min = average(A[0] + A[1], 2);
+
+//        for (int i = 0; i < n; i++) {
+//            int sum = A[0];
+//            int k = 1;
+//
+//            for (int j = i + 1; j < n; j++) {
+//                int b = B[i][j];
+//                sum += b;
+//                k++;
+//
+//                double avg = average(sum, k);
+//
+//                if (min > avg) {
+//                    min = avg;
+//                    answer = i;
+//                }
+//            }
+//        }
+
+        return answer;
+    }
+
+    private double average(int sum, int n) {
+        return (double) sum / n;
     }
 }
